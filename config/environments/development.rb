3d21251fb150baa48149fa46ae22a65a8e6bd6db
Toshiyuki023Hori <World.88Toshi@gmail.com>
2022-08-09 08:22:01 +0900

    Config: Set up redis for cache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,16 +16,20 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
-    }
-  else
-    config.action_controller.perform_caching = false
+  # redis 利用によりコメントアウト
+  # if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  #   config.cache_store = :memory_store
+  #   config.public_file_server.headers = {
+  #     'Cache-Control' => "public, max-age=#{2.days.to_i}"
+  #   }
+  # else
+  #   config.action_controller.perform_caching = false
+  #
+  #   config.cache_store = :null_store
+  # end
 
-    config.cache_store = :null_store
-  end
+  config.cache_store = :redis_store, "redis://redis:6379/0/cache"
+  config.active_record.cache_versioning = false
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
